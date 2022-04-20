090ee320859af9c1af91124a4d282822b47fa762
elvis <elvis.com.web.app@gmail.com>
2022-04-20 00:30:03 -0300

    add GoogleMap a MapDriver  19/04/2022

--- app/src/main/java/elvi/com/rapixtaxi/activities/driver/MapDriverActivity.java
+++ app/src/main/java/elvi/com/rapixtaxi/activities/driver/MapDriverActivity.java
@@ -1,16 +1,50 @@
 package elvi.com.rapixtaxi.activities.driver;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 import elvi.com.rapixtaxi.R;
+import elvi.com.rapixtaxi.activities.MainActivity;
+import elvi.com.rapixtaxi.activities.client.MapClientActivity;
+import elvi.com.rapixtaxi.providers.AuthProvider;
 
-public class MapDriverActivity extends AppCompatActivity {
+public class MapDriverActivity extends AppCompatActivity implements OnMapReadyCallback {
+    private  GoogleMap mMap;
+    private  SupportMapFragment mMapFragment;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map_driver);
+
+    mMapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+    mMapFragment.getMapAsync( this);
+
+
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+
+        mMap = googleMap;
+        mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+
     }
 }
+
+
+
+
