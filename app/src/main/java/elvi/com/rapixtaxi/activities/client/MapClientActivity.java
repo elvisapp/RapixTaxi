791df8671a3ab5551e05a91b82af2abbc807da67
elvis <elvis.com.web.app@gmail.com>
2022-04-21 00:53:40 -0300

    ajustes del map en MapDriver  20/04/2022

--- app/src/main/java/elvi/com/rapixtaxi/activities/client/MapClientActivity.java
+++ app/src/main/java/elvi/com/rapixtaxi/activities/client/MapClientActivity.java
@@ -1,38 +1,69 @@
 package elvi.com.rapixtaxi.activities.client;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
 
 import elvi.com.rapixtaxi.R;
 import elvi.com.rapixtaxi.activities.MainActivity;
 import elvi.com.rapixtaxi.providers.AuthProvider;
 
-public class MapClientActivity extends AppCompatActivity {
+public class MapClientActivity extends AppCompatActivity implements OnMapReadyCallback {
 
-    Button mButtonLogout;
 
     AuthProvider mAuthProvider;
+
+    private GoogleMap mMap;
+    private SupportMapFragment mMapFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map_client);
 
-        mButtonLogout = findViewById(R.id.btnLogout);
         mAuthProvider = new AuthProvider();
 
-        mButtonLogout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-              mAuthProvider.logout();
-                Intent intent = new Intent( MapClientActivity.this, MainActivity.class);
-                startActivity(intent);
-                finish();
-            }
-        });
+        mMapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mMapFragment.getMapAsync( this);
+
+
+    }
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+
+        mMap = googleMap;
+        mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.driver_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.action_logout) {
+            logout();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    void logout(){
+        mAuthProvider.logout();
+        Intent intent = new Intent( MapClientActivity.this, MainActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
--- app/src/main/java/elvi/com/rapixtaxi/activities/driver/MapDriverActivity.java
+++ app/src/main/java/elvi/com/rapixtaxi/activities/driver/MapDriverActivity.java
@@ -1,34 +1,88 @@
 package elvi.com.rapixtaxi.activities.driver;
 
+import android.Manifest;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Looper;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 import elvi.com.rapixtaxi.R;
 import elvi.com.rapixtaxi.activities.MainActivity;
 import elvi.com.rapixtaxi.activities.client.MapClientActivity;
+import elvi.com.rapixtaxi.includes.MyToolbar;
 import elvi.com.rapixtaxi.providers.AuthProvider;
 
 public class MapDriverActivity extends AppCompatActivity implements OnMapReadyCallback {
     private  GoogleMap mMap;
     private  SupportMapFragment mMapFragment;
+    private AuthProvider mAuthProvider;
+
+    private LocationRequest mLocationRequest;
+    private FusedLocationProviderClient mFusedLocation;
+
+    private final static int LOCATION_REQUEST_CODE = 1;
+
+
+
+    LocationCallback mLocationCallback = new LocationCallback() {
+
+        @Override
+        public void onLocationResult(LocationResult locationResult) {
+            for (Location location: locationResult.getLocations()){
+                if (getApplication() != null) {
+                    //localización en tiempo real del usuario
+
+                    mMap.moveCamera(CameraUpdateFactory.newCameraPosition(
+                            new CameraPosition.Builder()
+                                    .target(new LatLng(location.getLatitude(), location.getLongitude()))
+                                    .zoom(15f)
+                                    .build()
+                    ));
+
+                }
+
+            }
+        }
+    };
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map_driver);
+
+        MyToolbar.show( this, "Conductor", false);
+
+        mAuthProvider = new AuthProvider();
+        mFusedLocation = LocationServices.getFusedLocationProviderClient( this);
+
+
 
     mMapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
     mMapFragment.getMapAsync( this);
@@ -41,7 +95,88 @@ public class MapDriverActivity extends AppCompatActivity implements OnMapReadyCa
 
         mMap = googleMap;
         mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        mMap.getUiSettings().setZoomControlsEnabled(true);
 
+        mLocationRequest = new LocationRequest();
+        mLocationRequest.setFastestInterval(1000);
+        mLocationRequest.setFastestInterval(1000);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        mLocationRequest.setSmallestDisplacement(5);
+
+        startlocation();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == LOCATION_REQUEST_CODE) {
+             if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                 if (ContextCompat.checkSelfPermission( this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                     mFusedLocation.requestLocationUpdates(mLocationRequest, mLocationCallback, Looper.myLooper());
+                 }
+
+
+             }
+        }
+
+    }
+
+    private void startlocation() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                mFusedLocation.requestLocationUpdates(mLocationRequest, mLocationCallback, Looper.myLooper());
+            }
+            else {
+                checkLocationPermissions();
+            }
+        } else {
+            mFusedLocation.requestLocationUpdates(mLocationRequest, mLocationCallback, Looper.myLooper());
+
+        }
+    }
+    private  void checkLocationPermissions(){
+        if (ContextCompat.checkSelfPermission( this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)) {
+              new AlertDialog.Builder( this)
+                      .setTitle("Proporciona los permisos para continuar")
+                      .setMessage("Esta aplicación requiere de los permisos de ubicación para poder ser utilizada")
+                      .setPositiveButton("ok", new DialogInterface.OnClickListener() {
+                          @Override
+                          public void onClick(DialogInterface dialogInterface, int i) {
+                              ActivityCompat.requestPermissions( MapDriverActivity.this, new  String[] {Manifest.permission.ACCESS_FINE_LOCATION}, LOCATION_REQUEST_CODE);
+
+                          }
+                      })
+                      .create()
+                      .show();
+            }
+            else {
+                ActivityCompat.requestPermissions( MapDriverActivity.this, new  String[] {Manifest.permission.ACCESS_FINE_LOCATION}, LOCATION_REQUEST_CODE);
+
+            }
+        }
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.driver_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.action_logout) {
+            logout();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    void logout(){
+        mAuthProvider.logout();
+        Intent intent = new Intent( MapDriverActivity.this, MainActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
 
